aac5142c79f26255159f7cbbaf02cb54d647781f
Harshit Kamboj <harshitkamboj2110@gmail.com>
2018-02-24 09:30:01 +0530

    Update app.js
    NODE_ENV=Production

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+// setting node_env to production
+app.set('env','production');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
